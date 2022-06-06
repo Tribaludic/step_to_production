e7bf9dc86f912243c4cded7f1b41b7baf334ef34
Christian Bustamante <58147749+Tribaludic@users.noreply.github.com>
2022-06-06 11:40:25 -0500

    first app with bootstrap

---
+++ step-to-production/src/App.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container shadow-lg p-3 mt-2  bg-white rounded ">
+        <div className="row">
+          <div className="col my-auto">
+            <h5 className="p-3 mb-2 bg-dark text-white">REGISTRO PASO A PRODUCCION</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
